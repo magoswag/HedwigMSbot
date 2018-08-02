68496b6c4fe706c55e1c438375fc4b1cf93f0ae6
Mago <victor.elnen@gmail.com>
2018-08-02 23:13:38 +0200

    Update

--- HedwigMSbot.py
+++ HedwigMSbot.py
@@ -1,6 +1,5 @@
 import telegram
 from telegram.ext import *
-#Esto es pa actualiza
 
 mi_bot = telegram.Bot(token='682980920:AAGybjIygCGh3O6qXVdNCYgAazuJ8pBR6cc')
 mi_bot_updater = Updater(mi_bot.token)
@@ -8,17 +7,25 @@ mi_bot_updater = Updater(mi_bot.token)
 def listener(bot, update):
 	id=update.message.chat_id
 	mensaje=update.message.text
-	print('ID: '+ str(id) + (' Mensaje: ')+ mensaje)
+	hora=update.message.date
+	print('ID: '+ str(id) + (' Mensaje: ') + mensaje)
+	print('Hora del mensaje: ' + hora)
+	if 'hora' in mensaje:
+		bot.sendMessage(chat_id=id, text= 'Son las ')
 
 def start(bot, update, pass_chat_data=True):
 	id=update.message.chat_id
 	mensaje=update.message.text
-	bot.sendMessage(chat_id=id, text='Bienvenido! 😊')
+	if id==4432484:
+		bot.sendMessage(chat_id=id, text='Bienvenido, amo ⚡️')
+	else:
+		bot.sendMessage(chat_id=id, text='Bienvenido! 😊')
 
 def help(bot, update):
 	id=update.message.chat_id
 	mensaje=update.message.text
 	bot.sendMessage(chat_id=id, text='Los comandos que puedes utilizar conmigo son: /start /help')
+
 
 
 start_handler = CommandHandler('start',start)
