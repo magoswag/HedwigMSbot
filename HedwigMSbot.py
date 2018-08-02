9966a25e785557955b94697c3de3d69705d97b28
Mago <victor.elnen@gmail.com>
2018-08-03 00:05:41 +0200

    Update

--- HedwigMSbot.py
+++ HedwigMSbot.py
@@ -9,7 +9,9 @@ def listener(bot, update):
 	mensaje=update.message.text
 	hora=update.message.date
 	print('ID: '+ str(id) + (' Mensaje: ') + mensaje)
-	print('ID: '+str(id)+ 'Hora del mensaje: '+ str(hora))
+	print('ID: '+str(id)+ ' Hora del mensaje: '+ str(hora))
+	if 'hora' in mensaje:
+		bot.sendMessage(chat_id=id, text='Estamos a ' + str(hora))
 
 def start(bot, update, pass_chat_data=True):
 	id=update.message.chat_id
