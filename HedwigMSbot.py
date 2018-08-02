369e10f83c146f8899aef2eb6109e913c43d0edc
Mago <victor.elnen@gmail.com>
2018-08-02 15:51:09 +0200

    Update

--- HedwigMSbot.py
+++ HedwigMSbot.py
@@ -14,10 +14,7 @@ def listener(bot, update):
 def start(bot, update, pass_chat_data=True):
 	id=update.message.chat_id
 	mensaje=update.message.text
-	if id==4432484:
-		bot.sendMessage(chat_id=id, text='Bienvenido, amo ⚡️')
-	else:
-		bot.sendMessage(chat_id=id, text='Bienvenido! 😊')
+	bot.sendMessage(chat_id=id, text='Bienvenido! 😊')
 
 def help(bot, update):
 	id=update.message.chat_id
