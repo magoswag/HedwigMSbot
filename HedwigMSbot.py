db0013bc25d53e38c52f7d45e49cbfc2c7e77526
Mago <victor.elnen@gmail.com>
2018-12-25 19:36:45 +0100

    201812251936

--- HedwigMSbot.py
+++ HedwigMSbot.py
@@ -1,64 +1,59 @@
 import telegram
-from telegram.ext import *
+ from telegram.ext import *
 
 
-mi_bot = telegram.Bot(token='682980920:AAGybjIygCGh3O6qXVdNCYgAazuJ8pBR6cc')
-mi_bot_updater = Updater(mi_bot.token)
+ mi_bot = telegram.Bot(token='682980920:AAGybjIygCGh3O6qXVdNCYgAazuJ8pBR6cc')
+ mi_bot_updater = Updater(mi_bot.token)
 
-import logging
-logging.basicConfig(format=
-'%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+ import logging
+ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-
-def start(bot, update, pass_chat_data=True):  # Comando de bienvenida
-	id = update.message.chat_id
-	mensaje = update.message.text
-	if id == 4432484:
-		bot.sendMessage(chat_id=id, text=
-		'Bienvenido, amo ⚡️\n¿Qué haremos hoy?')
+ def start(bot, update, pass_chat_data=True): #Comando de bienvenida
+	id=update.message.chat_id
+	mensaje=update.message.text
+	if id==4432484:
+		bot.sendMessage(chat_id=id, text='Bienvenido, amo ⚡️\n¿Qué haremos hoy?')
 	else:
-		bot.sendMessage(chat_id=id, text=
-		'Bienvenido! 😊\nYo seré tu asistente personal. ¿Qué puedo hacer por ti?')
+		bot.sendMessage(chat_id=id, text='Bienvenido! 😊\nYo seré tu asistente personal. ¿Qué puedo hacer por ti?')
 
-# Comando para saber los comandos que puede ejecutar el bot
-def help(bot, update):
-	id = update.message.chat_id
-	mensaje = update.message.text
-	bot.sendMessage(chat_id=id,
-	text='Los comandos que puedes utilizar conmigo son: /start /help /fecha')
+def help(bot, update): #Comando para saber los comandos que puede ejecutar el bot
+	id=update.message.chat_id
+ 	mensaje=update.message.text
+ 	bot.sendMessage(chat_id=id, text='Los comandos que puedes utilizar conmigo son: /start /help /fecha')
 
-# Te dice el id, mensaje y la fecha a la que se ha enviado el mensaje
-def listener(bot, update):
-	id = update.message.chat_id
-	user = update.message.from_user
-	mensaje = update.message.text
-	#date1 = telegram.Message.date
-	date = str(update.message.date)
-	print('ID: ' + str(id) + ' Mensaje: ' + mensaje)
-	print('ID: ' + str(id) + ' Hora del mensaje: ' + date)
-	if ('Hola' in mensaje or 'hola' in mensaje) and len(mensaje) < 20:
-		bot.sendMessage(chat_id=id, text='Hola '+ user.first_name + '!')
-	if ('Fecha' in mensaje or 'fecha' in mensaje or 'Dia' in mensaje or 'dia' in mensaje)
-	and len(mensaje) <50:  # Para que te diga a que fecha estamos sin usar comando
-		bot.sendMessage(chat_id=id, text='Estamos a ' + date)
-
-# Comando para saber la fecha en la que estás
-def fecha(bot, update):
-	id = update.message.chat_id
-	fecha = str(update.message.date)
-	bot.sendMessage(chat_id=id, text='Estamos a ' + fecha)
+ def listener(bot, update): #Te dice el id, mensaje y la fecha a la que se ha enviado el mensaje
+ 	id=update.message.chat_id
+ 	user=update.message.from_user
+ 	mensaje=update.message.text
+ 	date=str(update.message.date)
+ 	print('ID: '+ str(id) + ' Mensaje: ' + mensaje)
+ 	print('ID: '+ str(id) + ' Hora del mensaje: '+ date)
+ 	if ('Hola' in mensaje or 'hola' in mensaje) and len(mensaje)<10:
+ 		bot.sendMessage(chat_id=id, text='Hola '+ user.first_name+'!')
+ 	if ('fecha' in mensaje or 'Fecha' in mensaje) and len(mensaje)<20: #Para que te diga a que fecha estamos sin usar comando
+ 		bot.sendMessage(chat_id=id, text='Estamos a '+ date)
 
 
-dp = mi_bot_updater.dispatcher
+ def fecha(bot, update): #Comando para saber la fecha en la que estás
+ 	id=update.message.chat_id
+ 	fecha=str(update.message.date)
+ 	bot.sendMessage(chat_id=id, text='Estamos a '+ fecha)
 
-addC('start', start)
-addM(Filters.text, listener)
-addC('help', help)
-addC('fecha', fecha)
+ start_handler = CommandHandler('start',start)
+ listener_handler = MessageHandler(Filters.text, listener)
+ help_handler= CommandHandler('help', help)
+ fecha_handler= CommandHandler('fecha', fecha)
+
+ dispatcher = mi_bot_updater.dispatcher
+
+ dispatcher.add_handler(start_handler)
+ dispatcher.add_handler(listener_handler)
+ dispatcher.add_handler(help_handler)
+ dispatcher.add_handler(fecha_handler)
 
 
-mi_bot_updater.start_polling()
-mi_bot_updater.idle()
+ mi_bot_updater.start_polling()
+ mi_bot_updater.idle()
 
-while True:
-	pass
+ while True:
+ 	pass
