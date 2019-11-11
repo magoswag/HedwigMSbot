2f71c31318809f7d13919e2f0986d84282f3ccda
magoswag <42012793+magoswag@users.noreply.github.com>
2019-11-11 22:53:57 +0100

    Update HedwigMSbot.py

--- HedwigMSbot.py
+++ HedwigMSbot.py
@@ -1,14 +1,14 @@
 import telegram
- from telegram.ext import *
+from telegram.ext import *
 
 
- mi_bot = telegram.Bot(token='682980920:AAGybjIygCGh3O6qXVdNCYgAazuJ8pBR6cc')
- mi_bot_updater = Updater(mi_bot.token)
+mi_bot = telegram.Bot(token='682980920:AAGybjIygCGh3O6qXVdNCYgAazuJ8pBR6cc')
+mi_bot_updater = Updater(mi_bot.token)
 
- import logging
- logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
- def start(bot, update, pass_chat_data=True): #Comando de bienvenida
+def start(bot, update, pass_chat_data=True): #Comando de bienvenida
 	id=update.message.chat_id
 	mensaje=update.message.text
 	if id==4432484:
@@ -21,7 +21,7 @@ def help(bot, update): #Comando para saber los comandos que puede ejecutar el bo
  	mensaje=update.message.text
  	bot.sendMessage(chat_id=id, text='Los comandos que puedes utilizar conmigo son: /start /help /fecha')
 
- def listener(bot, update): #Te dice el id, mensaje y la fecha a la que se ha enviado el mensaje
+def listener(bot, update): #Te dice el id, mensaje y la fecha a la que se ha enviado el mensaje
  	id=update.message.chat_id
  	user=update.message.from_user
  	mensaje=update.message.text
@@ -34,26 +34,26 @@ def help(bot, update): #Comando para saber los comandos que puede ejecutar el bo
  		bot.sendMessage(chat_id=id, text='Estamos a '+ date)
 
 
- def fecha(bot, update): #Comando para saber la fecha en la que estás
+def fecha(bot, update): #Comando para saber la fecha en la que estás
  	id=update.message.chat_id
  	fecha=str(update.message.date)
  	bot.sendMessage(chat_id=id, text='Estamos a '+ fecha)
 
- start_handler = CommandHandler('start',start)
- listener_handler = MessageHandler(Filters.text, listener)
- help_handler= CommandHandler('help', help)
- fecha_handler= CommandHandler('fecha', fecha)
+start_handler = CommandHandler('start',start)
+listener_handler = MessageHandler(Filters.text, listener)
+help_handler= CommandHandler('help', help)
+fecha_handler= CommandHandler('fecha', fecha)
 
- dispatcher = mi_bot_updater.dispatcher
+dispatcher = mi_bot_updater.dispatcher
 
- dispatcher.add_handler(start_handler)
- dispatcher.add_handler(listener_handler)
- dispatcher.add_handler(help_handler)
- dispatcher.add_handler(fecha_handler)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(listener_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_handler(fecha_handler)
 
 
- mi_bot_updater.start_polling()
- mi_bot_updater.idle()
+mi_bot_updater.start_polling()
+mi_bot_updater.idle()
 
- while True:
- 	pass
+while True:
+	pass
